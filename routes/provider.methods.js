2c0370425d8b639f200bc36a0b6c3e0e54e71d43
Jeffrey Campecino <jeffrey.campecino@gmail.com>
2017-08-14 01:03:51 -0700

    test

--- routes/provider.methods.js
+++ routes/provider.methods.js
@@ -100,13 +100,14 @@ class Provider {
   }
   // ADD EMPLOYEE SERVICE
   addService(request, response) {
-    let service = createService(request.body);
+    // let service = createService(request.body);
     // let set = {};
     // set[`services.${service._id}`] = service;
-    response.json(service);
+    // response.json(service);
     // _database.collection(employeeCollection).updateOne({ _id: ObjectId(request.body.employeeID) }, { $set: set }).then((data) => {
     //   response.json('Success!');
     // });
+    response.json('success!');
   }
   // EDIT EMPLOYEE SERVICE
   editService(request, response) {
