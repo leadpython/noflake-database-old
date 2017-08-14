6bd43793930116dcf6dec596f7e0d9a35e835e31
Jeffrey Campecino <jeffrey.campecino@gmail.com>
2017-08-14 00:38:56 -0700

    test

--- routes/provider.methods.js
+++ routes/provider.methods.js
@@ -65,10 +65,9 @@ class Provider {
   // GET EMPLOYEES
   getEmployees(request, response) {
     _database.collection(providerCollection).findOne({ _id: ObjectId(request.params.providerID) }, { employees: true }).then((data) => {
-      response.json(keysToArray(data.employees));
-      // _database.collection(employeeCollection).find({ _id: { $in: keysToArray(data.employees) } }).toArray((error, data) => {
-      //   response.json(data);
-      // });
+      _database.collection(employeeCollection).find({ _id: { $in: keysToArray(data.employees) } }).toArray((error, data) => {
+        response.json(data);
+      });
     });
   }
   // ADD EMPLOYEE
